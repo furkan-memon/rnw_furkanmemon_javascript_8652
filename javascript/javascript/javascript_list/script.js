f9b66f1f4b4f4cf7b7cc6d746e9486c4e85a79f4
furkan_memon <furkanmemon860@gmail.com>
2025-09-07 22:36:18 +0530

    7-9-25

--- javascript/javascript/javascript_list/script.js
+++ javascript/javascript/javascript_list/script.js
@@ -42,12 +42,119 @@
      console.log("Area of triangle is : ", area)
 }
 // JavaScript Program to Swap Two Variables
-let a = 5;
-let b= 10;
- console.log("Before swapping: a = " + a + ", b = " + b);
+{
 
-let temp =a ;
-a =  b ;
-b = temp;
+    let a = 5;
+    let b= 10;
+     console.log("Before swapping: a = " + a + ", b = " + b);
+    
+    let temp =a ;
+    a =  b ;
+    b = temp;
+    
+    console.log("After swapping: a = " + a + ", b = " + b);
+}
+// JavaScript Program to Convert Kilometres to Miles
+{
+    let Kilometres = 10;
+    let miles = Kilometres * 0.621371;
+    console.log(Kilometres + " kilometers is equal to " + miles + " miles.");
+}
+// Javascript Program to Convert Celsius to Fahrenheit
+{
+    let celsius = 25;
+    let Fahrenheit = (celsius * 9/5) + 32;
+    console.log( celsius + "°C is equal to " + Fahrenheit + "°F." );
+}
+// Javascript Program to Check if a number is Positive, Negative, or Zero
+{
+    let num = parseInt(prompt("Enter a number: "));
+    if (num >0){
+        console.log("The number is positive.");
+    }
+    else if(num<0){
+        console.log("the number is negative.");
+    }
+    else{
+        console.log("The number is zero.");
+    }
+}
+// Javascript Program to Check if a Number is Odd or Even
+{
+    let number = parseInt(prompt("Enter a number: "));
+    if (number % 2 === 0) {
+        console.log(number + " is an even number.");
+    } else {
+        console.log(number + " is an odd number.");
+    }
+}
+// JavaScript Program to Find the Largest Among Three Numbers
+{
+     let num1 = parseFloat(prompt("Enter first number: "));
+    let num2 = parseFloat(prompt("Enter second number: "));
+    let num3 = parseFloat(prompt("Enter third number: "));
+    let largest;
 
-console.log("After swapping: a = " + a + ", b = " + b);
+    if (num1 >= num2 && num1 >= num3) {
+        largest = num1;
+    } else if (num2 >= num1 && num2 >= num3) {
+        largest = num2;
+    } else {
+        largest = num3;
+    }
+    console.log("The largest number is: " + largest);
+}
+// JavaScript Program to Check Prime Number
+{
+    let number = parseInt(prompt("Enter a positive integer: "));
+    let isPrime = true;
+    if (number <= 1) {
+        isPrime = false;
+    } else {
+        for (let i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i === 0) {
+                isPrime = false;
+                break;
+            }
+        }
+}
+    if (isPrime) {
+        console.log(number + " is a prime number.");
+    }
+    else {
+        console.log(number + " is not a prime number.");
+    }
+}
+// JavaScript Program to Print All Prime Numbers in an Interval
+{
+    let lower = parseInt(prompt("Enter the lower bound: "));
+    let upper = parseInt(prompt("Enter the upper bound: "));
+    console.log("Prime numbers between " + lower + " and " + upper + " are:");
+    for (let num = lower; num <= upper; num++) {
+        let isPrime = true;
+        if (num <= 1) {
+            isPrime = false;
+        } else {
+            for (let i = 2; i <= Math.sqrt(num); i++) {
+                if (num % i === 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+        if (isPrime) {
+            console.log(num);
+
+        }
+    }
+}
+{
+    let number = parseInt(prompt("Enter a positive integer: "))
+    let  sum = 1;
+    for (let i = 1; i <= number; i++) {
+   sum =sum*i ;
+        
+}
+console.log(sum);
+
+}
