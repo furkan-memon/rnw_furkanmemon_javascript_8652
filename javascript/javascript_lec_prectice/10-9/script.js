1afaf38eb99fcd9a5050208a5f75f507cc8db5e8
furkan_memon <furkanmemon860@gmail.com>
2025-09-10 21:26:46 +0530

    9-10

---
+++ javascript/javascript_lec_prectice/10-9/script.js
@@ -0,0 +1,12 @@
+function vowels(str){
+    let count = 0
+    for(let i=0;i<str.length;i++){
+        if(str[i]=='a' || str[i]=='e' || str[i]=='i' || str[i]=='o' || str[i]=='u' || str[i]=='A' || str[i]=='E' || str[i]=='I' || str[i]=='O' || str[i]=='U'){
+            count++
+        }
+    }
+    return count
+}
+let str = prompt("Enter a string :")
+let result = vowels(str)
+console.log("Number of vowels in the string is : " + result);
