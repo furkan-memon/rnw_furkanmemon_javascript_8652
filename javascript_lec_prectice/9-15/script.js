e4ce33f27be763afa4134c418f97f1b468e2f28a
furkan_memon <furkanmemon860@gmail.com>
2025-09-15 22:26:16 +0530

    9-15

---
+++ javascript_lec_prectice/9-15/script.js
@@ -0,0 +1,13 @@
+// Date and time in JavaScript
+let now = new Date();
+console.log("Current date and time:", now);
+console.log("Year:", now.getFullYear());
+console.log("Month:", now.getMonth() + 1);
+console.log("Date:", now.getDate());
+console.log("Hours:", now.getHours());
+console.log("Minutes:", now.getMinutes());
+console.log("Seconds:", now.getSeconds());
+ console.log("getTime:", now.getTime());
+console.log("toLocaleString:", now.toLocaleString());
+console.log("toDateString:", now.toDateString());
+console.log("toTimeString:", now.toTimeString());
