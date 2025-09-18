01cd9c664cf553ac9c8ac53736071d1ea9a0c277
furkan_memon <furkanmemon860@gmail.com>
2025-09-18 21:04:40 +0530

    9-18

---
+++ javascript_lec_prectice/9-18/script.js
@@ -0,0 +1,101 @@
+// {
+//   let arr = [1,2,3,4,5,6,7,8,9]
+//   let result = arr.at(5)
+//   console.log(result);
+// }
+// {
+//   let arr = ['🍕','🍔','🍟','🌭','🍿','🧀','🍩','🎂','🍰','☕','🌮','🍜','🍽','🧂']
+
+//   console.log(arr);
+
+// //   let result1 = arr.copyWithin(3)
+
+// //   console.log(result1);
+
+// //   let result2 = arr.copyWithin(3 , 7)
+
+// //   console.log(result2);
+  
+//   let result3 = arr.copyWithin(3 , 5 , 8)
+
+//   console.log(result3);
+  
+// }
+// {
+//   // let arr = ['🍕','🍔','🍟','🌭','🍿','🧀','🍩','🎂','🍰','☕','🌮','🍜','🍽','🧂']
+
+//   // let result = arr.entries()
+
+//   // console.log(result.next().value);
+//   // console.log(result.next().value);
+//   // console.log(result.next().value);
+//   // console.log(result.next().value);
+//   // console.log(result.next().value);
+//   // console.log(result.next().value);
+//   // console.log(result.next().value);
+//   // console.log(result.next().value);
+//   // console.log(result.next().value);
+//   // console.log(result.next().value);
+//   // console.log(result.next().value);
+//   // console.log(result.next().value);
+//   // console.log(result.next().value);
+//   // console.log(result.next().value);
+//   // console.log(result.next().value);
+  
+// }
+// {
+
+//   const data = (item) => item < 50
+
+//   const array1 = [1, 30, 39, 29, 10, 13 , 56];
+
+//   console.log(array1.every(data));
+
+// }
+// {
+  // const array = [1, 2, 3, 4];
+
+  // // let result = array.fill('a')
+  // // let result = array.fill('a' , 2)
+  // // let result = array.fill('a' , 1 , 3)
+
+  // console.log(result);
+  
+// }
+// {
+//   const words = ["spray", "elite", "exuberant", "destruction", "present"];
+
+//   const profiles  = [
+//     {
+//       name:'rohit',
+//       age:12
+//     },
+//       {
+//       name:'ronak',
+//       age:20
+//     },
+//       {
+//       name:'roshan',
+//       age:45
+//     },
+//       {
+//       name:'romil',
+//       age:30
+//     },
+//       {
+//       name:'rohan',
+//       age:23
+//     },
+//       {
+//       name:'roman',
+//       age:12
+//     },
+//   ]
+
+//   // const result = words.filter(w => w.length > 5 && w.length < 8 )
+
+//   let result = profiles.filter((age) => age.age == 12).map((name) => name.name)
+
+//   console.log(result);
+  
+// }
