dee9942d2a8397b983456efc1720486360bd8c3c
furkan_memon <furkanmemon860@gmail.com>
2025-09-19 21:16:23 +0530

    9-19

---
+++ javascript_lec_prectice/9-19/script.js
@@ -0,0 +1,69 @@
+// find()
+{
+    let arr = [10,18,20,25,40,52]
+    let risulte = arr.find((value) => value < 18)
+  console.log(risulte);
+}
+// findIndex()
+{
+      let arr = [10,18,20,25,40,52]
+    let risulte = arr.findIndex((value) => value < 18)
+  console.log(risulte);
+}
+// findelast()
+{
+      let arr = [10,18,20,25,40,52]
+    let risulte = arr.findLast((value) => value > 10)
+  console.log(risulte);
+}
+// findLastIndex()
+{
+      let arr = [10,18,20,25,40,52]
+    let risulte = arr.findLastIndex((value) => value > 10)
+  console.log(risulte);
+}
+{
+      let arr = [10,18,20,25,40,52]
+    let risulte = arr.push("aman")
+  console.log(risulte);
+  console.log(arr); 
+  
+
+}
+{
+
+    let arr = [10,18,20,25,40,52]
+        let risulte = arr.pop("aman")
+      console.log(risulte);
+      console.log(arr); 
+}
+{
+    let arr = [10,18,20,25,40,52]
+        let risulte = arr.unshift("aman")
+      console.log(risulte);
+      console.log(arr); 
+}
+{
+    let arr = [10,18,20,25,40,52]
+        let risulte = arr.shift("aman")
+      console.log(risulte);
+      console.log(arr); 
+}
+{
+       let arr = [10,18,20,25,40,52]
+        let risulte = arr.includes(19)
+      console.log(risulte);
+      console.log(arr);
+}
+{
+    let anime = ["op","aot" ,"dbz","hXh","jjk"]
+    let risulte = anime.indexOf("aot")
+    console.log(risulte);
+    
+}
+{
+    let anime = ["op","aot" ,"dbz","op","jjk"]
+    let risulte = anime.lastIndexOf("op")
+    console.log(risulte);
+    
+}
