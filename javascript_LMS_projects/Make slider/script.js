689690028570be8ed9054f45d517f70d1ec7ebd7
furkan_memon <furkanmemon860@gmail.com>
2025-10-15 18:28:54 +0530

    10-15(6:28)

--- javascript_LMS_projects/Make slider/script.js
+++ javascript_LMS_projects/Make slider/script.js
@@ -1,27 +1,28 @@
-let index = 0;
-const slides = document.querySelectorAll(".slide");
-const dots = document.querySelectorAll(".dot");
+  let index = 0;
+    let slides = document.getElementsByClassName("slide")
+    let dots = document.getElementsByClassName("dot")
 
-function showSlide(i) {
-  slides.forEach(slide => slide.classList.remove("active"));
-  dots.forEach(dot => dot.classList.remove("active"));
-  slides[i].classList.add("active");
-  dots[i].classList.add("active");
-}
+    function showSlide(i) {
+      for (let s = 0; s < slides.length; s++) {
+        slides[s].classList.remove("active")
+        dots[s].classList.remove("active")
+      }
+      slides[i].classList.add("active")
+      dots[i].classList.add("active")
+    }
 
-function nextSlide() {
-  index = (index + 1) % slides.length;
-  showSlide(index);
-}
+    function nextSlide() {
+      index = (index + 1) % slides.length
+      showSlide(index)
+    }
 
-function prevSlide() {
-  index = (index - 1 + slides.length) % slides.length;
-  showSlide(index);
-}
+    function prevSlide() {
+      index = (index - 1 + slides.length) % slides.length
+      showSlide(index)
+    }
 
-function goToSlide(i) {
-  index = i;
-  showSlide(index);
-}
-
-setInterval(nextSlide, 5000);
+    function goToSlide(i) {
+      index = i
+      showSlide(index)
+    }
+    setInterval(nextSlide, 5000)
