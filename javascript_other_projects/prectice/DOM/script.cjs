f74a7bdd070e3743e0e06564970ba8a2dd2637fb
furkan_memon <furkanmemon860@gmail.com>
2025-10-23 21:12:33 +0530

    10-23

--- javascript_other_projects/prectice/DOM/script.cjs
+++ javascript_other_projects/prectice/DOM/script.cjs
@@ -41,6 +41,82 @@
     }
     {
         let abcd2 = document.querySelector(".link1")
-      abcd2.getAttribute("href")
+     console.log(abcd2.getAttribute("href"));
+     
     }
+    {
+        let abcd2 = document.querySelector(".link1")
+        console.log(abcd2.removeAttribute("href"));
+    }
+        
+}
+// DOM manipulation
+{
+    // createElement and appendChild
+    {
+       let el = document.createElement("p")
+        el.textContent = "Kids who never seen peace and kids who never seen war have different values."
+       document.body.prepend(el)
+       document.querySelector("#kids").prepend(el)
+    //    appendChild means add at last
+    //   prepend means add at first
+    }
+    // remove
+    {
+
+        let parent = document.querySelector("#kids")
+        
+        let child = document.querySelector("#child")
+        // parent.removeChild(child)
+    // alternative of removeChild   
+    //    parent.remove()
+    }
+    // style
+    {
+let name = document.querySelector("#child")
+name.style.color = "#FFF2EF"
+name.style.backgroundColor = "#1A2A4F"
+name.style.margin = "10px"
+name.style.padding = "10px"
+name.style.borderRadius = "100%"
+name.style.display = "flex"
+name.style.alignItems = "center"
+name.style.fontSize = "15px"
+name.style.height = "100px"
+name.style.width = "100px"
+
+
+
+}
+// CLASSLIST
+{
+    let luffy = document.querySelector(".luffy")
+    luffy.classList.add("lu")
+    luffy.classList.remove("luffy")
+    luffy.classList.toggle, ("luffy")
+     
+}
+}
+
+{
+  {
+    let heading = document.getElementById("heading")
+        heading.textContent = "Monky D luffy welcom to my ship"
+    
+  }
+  {
+    let lii = document.querySelectorAll("li")
+   lii.forEach(function(val) {
+    console.log(val.textConten
+        
+    );
+
+   });
+   for(let i = 0 ; i<lii.length;i++)
+   {
+    console.log(lii[i].textContent);
+    
+   }
+  }
+
 }
