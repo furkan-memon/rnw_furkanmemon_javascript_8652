cfec5e50cc238fe2c12bbd1863c8d68395684750
furkan_memon <furkanmemon860@gmail.com>
2025-11-18 21:35:20 +0530

    11-18

--- javascript_lec_prectice/11-18/script.js
+++ javascript_lec_prectice/11-18/script.js
@@ -1,38 +1,57 @@
 $(function () {
     let $box = $('#box');
     let $box2 = $('#box2');
-
+  let $playButton = $('#animateMoveBtn') 
+  let $audio = $('#myAudio') 
 
     $('#fadeOutBtn').on('click', function () {
+          $audio[0].currentTime = [0];
+         $audio[0].play();
         $box.fadeOut('slow');
     });
     $('#fadeInBtn').on('click', function () {
+          $audio[0].currentTime = [0];
+         $audio[0].play();
         $box.fadeIn('slow');
     });
     $('#fadeToggleBtn').on('click', function () {
+          $audio[0].currentTime = [0];
+         $audio[0].play();
         $box.fadeToggle(1000);
     });
     $('#fadeToBtn').on('click', function () {
-        $box.fadeTo(700, 0.3,);
+          $audio[0].currentTime = [0];
+         $audio[0].play();
+                 $box.fadeTo(700, 0.3,);
     });
     $('#fadeResetBtn').on('click', function () {
+          $audio[0].currentTime = [0];
+         $audio[0].play();
         $box.stop(true).css({ opacity: 1 }).show();
     });
 
 
     $('#animateMoveBtn').on('click', function () {
-        $box2.css('left', 0);
+        // $box2.css('left', 0);
+        $audio[0].currentTime = [0];
+         $audio[0].play();
         $box2.animate({
+           
+            // left:"200px",
             top:'200px',
             width: '180px',
             height: '80px',
             opacity: 0.85
         },
         {
-            duration: 800,
+            duration: 1000,
             easing: 'swing',
             complete: function () {
-                $(this).animate({ left: '0px', top:'0' , width: '120px', height: '120px', opacity: 1 }, 600, 'swing');
+                $(this).animate({ left: '200px', top:'200' , width: '120px', height: '80px', opacity: 1 }, 600, 'swing');
+                $(this).animate({ left: '200px', top:'200' , width: '120px', height: '80px', opacity: 1 }, 600, 'swing');
+                $(this).animate({ left: '0px', top:'200',right:'100px' , width: '120px', height: '120px', opacity: 1 }, 600, 'swing');
+                $(this).animate({ left: '0px', top:'0',right:'0px' , width: '120px', height: '120px', opacity: 1 }, 600, 'swing');
+                
             }
         }
     );
@@ -40,7 +59,9 @@ $(function () {
 
 
     $('#animateResetBtn').on('click', function () {
-        $box2.stop(true).css({ left: 0,  top:0 , width: '120px', height: '120px', opacity: 1 });
+          $audio[0].currentTime = [0];
+         $audio[0].play();
+        $box2.stop(true).css({ left: 0,right:'0px',  top:0 , width: '120px', height: '120px', opacity: 1 });
 
     });
 
