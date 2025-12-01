fd383329ae93c10b6844d224be8db780cbff2a2e
furkan_memon <furkanmemon860@gmail.com>
2025-12-01 22:43:01 +0530

    12-1
    Add file input handling and update button text/icon

--- javascript_other_projects/File Uploader/script.cjs
+++ javascript_other_projects/File Uploader/script.cjs
@@ -1,2 +1,29 @@
         let btn = document.querySelector(".btn")
+        let inp = document.querySelector(".inp")
         
+        
+        const btnText = document.getElementById("btn-text");
+        const btnIcon = document.getElementById("btn-icon");
+
+        btn.addEventListener("click",function (){
+            inp.click()
+        })
+
+        inp.addEventListener("change",function(val){
+            let file = val.target?.files[0]
+            
+            if(file){
+               
+                btnText.textContent = file.name
+                btnIcon.textContent = "✅" 
+                btn.classList.add("file-selected")
+            } else {
+               
+                btnText.textContent = "Select File to Upload"
+                btnIcon.textContent = "📁" 
+                btn.classList.remove("file-selected")
+            }
+
+        })
+        
+
